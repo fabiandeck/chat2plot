f0f32f9a721a1806328aed41667a431058982e09
Fabian Deck <104829191+fabiandeck@users.noreply.github.com>
2024-07-15 14:58:29 +0000

    update ChatOpenAI

--- chat2plot/chat2plot.py
+++ chat2plot/chat2plot.py
@@ -10,7 +10,7 @@ import commentjson
 import jsonschema
 import pandas as pd
 import pydantic
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain.chat_models.base import BaseChatModel
 from langchain.schema import BaseMessage, FunctionMessage, HumanMessage, SystemMessage
 from plotly.graph_objs import Figure
